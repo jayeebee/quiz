a3eba3343824e1c6ab09e77c9cf1a79788cdca56
jayeebee <pnuthexsa311@gmail.com>
2021-03-12 11:05:56 -0500

    button text and question text added

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var startButton = document.getElementById('start-button')
 //the answer field, starts hidden will be show on click
 var questionContainerEl = document.getElementById('result')
-var questionEl = document.getElementById('question-bank')
+var questionEl = document.getElementById('quiz-container')
 var answerEl = document.getElementById('choice')
 
 startButton.addEventListener('click', startGame);
@@ -13,9 +13,20 @@ function startGame() {
     startButton.classList.remove('start')
     startButton.classList.add('hide')
     questionContainerEl.classList.remove('hide')
+    questionEl.classList.remove('hide')
+ 
+
+    document.querySelector("#choice0").textContent=questions[0].answers[0].text;
+    document.querySelector("#choice1").textContent=questions[0].answers[1].text;
+    document.querySelector("#choice2").textContent=questions[0].answers[2].text;
+    document.querySelector("#choice3").textContent=questions[0].answers[3].text;
+    document.querySelector("#question-ask").textContent=questions[0].question;
+
+   
+
 
     //start timer
-    var timeleft = 5;
+    var timeleft = 60;
     var downloadTimer = setInterval(function(){
         if(timeleft <= 0){
         clearInterval(downloadTimer);
@@ -29,50 +40,48 @@ function startGame() {
 
     console.log('started')
 
-
 };
 
 
-function showQuestion() {
-    questionEl.innerText = questions.question
-    console.log("question maker")
-}
-
 var questions = [
     {
-    question: "_______ tag is an extension to HTML that can enclose any number of Javascript statements", 
+    question: ' Which tag is an extension to HTML that can enclose any number of Javascript statements' ,
     answers: [
         { text: '<SCRIPT>', correct: true }, 
         { text: '<BODY>', correct: false },
         { text: '<HEAD>', correct: false },
         { text: '<TITLE>', correct: false}],
-    
-    
-    question: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?", 
+    },
+    {
+    question: ' Which built-in method adds one or more elements to the end of an array and returns the new length of the array?', 
     answers: [
         { text : 'last()', correct: false}, 
         { text : 'put()', correct: false}, 
         { text : 'push()', correct: true}, 
         { text : 'None of the above', correct: false}], 
-    
-    question: "Which built-in method returns the calling string value converted to upper case?",
+    },
+    {
+    question: ' Which built-in method returns the calling string value converted to upper case?',
     answers: [
         { text : 'toUpperCase', correct: true},
         { text : 'toUpper()', correct: false},
         { text : 'changeCase(case)', correct: false},
         { text : 'None of the above', correct: false}], 
-    
-    question: "Which of the following function of String object combines the text of two strings and returns a new string?", 
+    },
+    {
+    question: ' Which of the following function of String object combines the text of two strings and returns a new string?', 
     answers: [
         { text : 'add()', correct: false},
         { text : 'merge()', correct: false},
         { text : 'concat()', correct: true},
         { text : 'append()', correct: false}],
-    
-    question: "JavaScript is a ___ -side programming language.", 
+    },
+    {
+    question: ' JavaScript is a ___ -side programming language.', 
     answers: [
         { text : 'Client',  correct: false},
         { text : 'Server',  correct: false},
         { text : 'Both',  correct: true},
         { text : 'Neither', correct: false}]
-    }]
+    }
+]
