c0ff49c345c5fa2b14737fd8081a704fe8a07939
jayeebee <pnuthexsa311@gmail.com>
2021-03-14 14:01:49 -0400

    added partially working next question functionality and time loss

--- assets/script.js
+++ assets/script.js
@@ -4,43 +4,112 @@ var startButton = document.getElementById('start-button')
 var questionContainerEl = document.getElementById('result')
 var questionEl = document.getElementById('quiz-container')
 var answerEl = document.getElementById('choice')
+var buttonEl = document.getElementById('button0')
 
+
+
+let numCorrect = 0;
+    
 startButton.addEventListener('click', startGame);
 
 function startGame() {
 
-    //remove class from start button and add hidden display, remove hidden display from questions(maybe?) and answers
+    //remove class from start button and add hidden display, remove hidden display from question and answers
     startButton.classList.remove('start')
     startButton.classList.add('hide')
     questionContainerEl.classList.remove('hide')
     questionEl.classList.remove('hide')
  
-
+    //populate question field and answer buttons
     document.querySelector("#choice0").textContent=questions[0].answers[0].text;
     document.querySelector("#choice1").textContent=questions[0].answers[1].text;
     document.querySelector("#choice2").textContent=questions[0].answers[2].text;
     document.querySelector("#choice3").textContent=questions[0].answers[3].text;
     document.querySelector("#question-ask").textContent=questions[0].question;
 
-   
-
-
     //start timer
-    var timeleft = 60;
-    var downloadTimer = setInterval(function(){
-        if(timeleft <= 0){
-        clearInterval(downloadTimer);
+    var timeLeft = 60;
+    var quizTimer = setInterval(function(){
+        if(timeLeft <= 0){
+        clearInterval(quizTimer);
         document.getElementById("time").innerHTML = "0 sec";
         window.alert("Times Up! Game Over!")
         } else {
-        document.getElementById("time").innerHTML = timeleft + " sec";
+        document.getElementById("time").innerHTML = timeLeft + " sec";
         }
-        timeleft -= 1;
+        timeLeft -= 1;
     }, 1000);
+
+    let i = 0;
+
+    button0.addEventListener('click', function(){
+        if(questions[i].answers[0].correct){
+        window.alert('Correct')
+        numCorrect++;
+        console.log(numCorrect);
+        } else  {timeLeft -= 5;
+        window.alert('Incorrect') 
+        }   
+        document.querySelector("#choice0").textContent=questions[i].answers[0].text;
+        document.querySelector("#choice1").textContent=questions[i].answers[1].text;
+        document.querySelector("#choice2").textContent=questions[i].answers[2].text;
+        document.querySelector("#choice3").textContent=questions[i].answers[3].text;
+        document.querySelector("#question-ask").textContent=questions[i].question;
+        i++;
+    } 
+    )
+
+    button1.addEventListener('click', function(){
+        if(questions[i].answers[1].correct){
+        window.alert('Correct')
+        numCorrect++;
+        } else  {timeLeft -= 5;
+        window.alert('Incorrect') 
+        }
+        document.querySelector("#choice0").textContent=questions[i].answers[0].text;
+        document.querySelector("#choice1").textContent=questions[i].answers[1].text;
+        document.querySelector("#choice2").textContent=questions[i].answers[2].text;
+        document.querySelector("#choice3").textContent=questions[i].answers[3].text;
+        document.querySelector("#question-ask").textContent=questions[i].question;   
+        i++;
+    } 
+    )
+        button2.addEventListener('click', function(){
+        if(questions[i].answers[2].correct){
+        window.alert('Correct')
+        numCorrect++;
+        } else  {timeLeft -= 5;
+        window.alert('Incorrect') 
+        }   
+        document.querySelector("#choice0").textContent=questions[i].answers[0].text;
+        document.querySelector("#choice1").textContent=questions[i].answers[1].text;
+        document.querySelector("#choice2").textContent=questions[i].answers[2].text;
+        document.querySelector("#choice3").textContent=questions[i].answers[3].text;
+        document.querySelector("#question-ask").textContent=questions[i].question;
+        i++
+    } 
+    )
+        button3.addEventListener('click', function(){
+        if(questions[i].answers[3].correct){
+        window.alert('Correct')
+        numCorrect++;
+        } else  {timeLeft -= 5;
+        window.alert('Incorrect') 
+        }   
+        document.querySelector("#choice0").textContent=questions[i].answers[0].text;
+        document.querySelector("#choice1").textContent=questions[i].answers[1].text;
+        document.querySelector("#choice2").textContent=questions[i].answers[2].text;
+        document.querySelector("#choice3").textContent=questions[i].answers[3].text;
+        document.querySelector("#question-ask").textContent=questions[i].question;
+        i++
+    } 
+    )
 
     console.log('started')
 
 };
+
+
 
 
 var questions = [
